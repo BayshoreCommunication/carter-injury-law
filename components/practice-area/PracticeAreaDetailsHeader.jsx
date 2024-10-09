0acae6b7b28eb527eb6b7ade8ecbc5423683a021
arsahak <arsahak99@gmail.com>
2024-10-09 10:47:36 +0600

    update service data

--- components/practice-area/PracticeAreaDetailsHeader.jsx
+++ components/practice-area/PracticeAreaDetailsHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import HeaderForm from "../shared/HeaderForm";
 import SectionLayout from "../shared/SectionLayout";
 
-const PracticeAreaHeader = () => {
+const PracticeAreaDetailsHeader = ({ title }) => {
   return (
     <div className="">
       <div className="relative w-full h-[400px] md:h-[500px] lg:h-[750px]">
@@ -19,21 +19,12 @@ const PracticeAreaHeader = () => {
           <h2
             className={`text-white font-semibold text-3xl md:text-5xl lg:text-[80px]  text-center uppercase drop-shadow-[1px_0_10px_#000000]`}
           >
-            Car Accident
+            {title}
           </h2>
         </div>
-
-        <div className="absolute -bottom-52 right-[8%] hidden lg:block">
-          <HeaderForm className={" bg-white"} />
-        </div>
-      </div>
-      <div className="block md:hidden">
-        <SectionLayout>
-          <HeaderForm className="bg-white" />
-        </SectionLayout>
       </div>
     </div>
   );
 };
 
-export default PracticeAreaHeader;
+export default PracticeAreaDetailsHeader;
