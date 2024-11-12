77badf70bfdab525ae013fd11620b8c6014d29a3
arsahak <arsahak99@gmail.com>
2024-11-12 10:45:21 +0600

    add service image

---
+++ config/sahakJson.js
@@ -0,0 +1,243 @@
+[
+  {
+    icon: "/assets/servicepage/icon/auto-accidents.png",
+    title: "Physical Abuse",
+    slug: "physical-abuse",
+    details: `<div className="w-full flex flex-col gap-2 lg:gap-4">
+<h2 className="text-stone-950 font-semibold text-3xl md:text-4xl lg:text-5xl">
+    Physical Abuse
+</h2>
+<img
+  src="/assets/servicepage/nursing-home-abuse/physical-abuse.jpg"
+  alt="Physical Abuse"
+  className="w-full py-3"
+/>
+
+<p className="text-base md:text-lg">
+At nursing homes, physical abuse occurs when residents are harmed by deliberate force or rough handling, such as hitting, slapping, or improper restraint. Florida law enforces that nursing homes provide care safely and respectfully. Violating these duties, resulting in physical harm is grounds for legal action.
+</p>
+
+<p class= "text-base md:text-lg font-bold mb-1  pl-5">
+    Who Should File a Claim and Why Having an Attorney Is Essential 
+</p>
+
+<p class="text-base md:text-lg ">
+ <ul class="list-disc pl-5">
+<li><span class= " font-bold ">Benefits of Filing:</span> Victims of nursing home physical abuse and their families can receive compensation for medical costs, pain, emotional distress, and possible other legal damages. Taking legal action also helps promote higher care standards and safety in nursing homes
+</li>
+
+ <li><span class= " font-bold ">Consequences Without Representation:</span> Without an experienced attorney, proving nursing home abuse can be challenging. Facilities may deny or downplay allegations, and crucial evidence can be lost. Florida law sets a two-year statute of limitations for abuse claims. Failing to meet deadlines or establish a strong case can mean abuse continues and victims receive no justice
+  </li>
+ </ul>
+</p>
+
+
+<p class="text-base md:text-lg font-bold mb-1  pl-5">
+ Carter Injury Law: Committed to Securing the Justice You Deserve
+</p>
+
+<p className="text-base md:text-lg">
+  <ul class="list-disc pl-5">
+
+  <li>We’ll gather key evidence, including medical records, staff reports, security footage, and witness testimonies, to fully uncover the abuse and identify those responsible
+  </li>
+
+  <li>Establishing that another party’s negligence led to your injuries requires detailed evidence, from witness statements to expert reports. Without an attorney, obtaining and organizing this evidence can be overwhelming.
+  </li>
+
+  <li>Our team is deeply familiar with Florida’s nursing home laws and will use this expertise to hold the facility accountable for its failures.
+  </li>
+
+  <li><span>We’ll handle negotiations with insurance companies and nursing home operators on your behalf to ensure the full compensation you deserve.
+</span></li>
+
+  <li>We offer a free, private consultation to help individuals navigate the emotional impact of physical abuse, with no financial obligation unless we win.
+  </li>
+
+  </ul>
+</p>
+
+</div>
+`,
+  },
+
+  {
+    icon: "/assets/servicepage/icon/auto-accidents.png",
+    title: "Neglect and Abandonment",
+    slug: "neglect-and-abandonment",
+    details: `<div className="w-full flex flex-col gap-2 lg:gap-4">
+<h2 className="text-stone-950 font-semibold text-3xl md:text-4xl lg:text-5xl">
+Neglect and Abandonment
+</h2>
+<img
+  src="/assets/servicepage/nursing-home-abuse/neglect-and-abandonment.jpg"
+  alt="Neglect and Abandonment"
+  className="w-full py-3"
+/>
+
+<p className="text-base md:text-lg">
+In nursing homes, neglect and abandonment mean that residents don’t receive the necessary care or supervision they need. Neglect includes failure to provide food, water, medication, hygiene, or mobility assistance, while abandonment involves leaving a resident unattended for extended periods, creating dangerous situations - even life risk. Florida law mandates that nursing homes provide adequate care.
+</p>
+
+<p class= "text-base md:text-lg font-bold mb-1  pl-5">
+    Who Should File a Claim and Why Having an Attorney Is Essential 
+</p>
+
+<p class="text-base md:text-lg ">
+ <ul class="list-disc pl-5">
+<li><span class= " font-bold ">Benefits of Filing:</span> Families of nursing home residents affected by neglect or abandonment can seek compensation for medical bills, pain and suffering, emotional distress, and loss of quality of life. If the neglect leads to long-term health issues, they may also be entitled to damages for future care costs.
+</li>
+
+ <li><span class= " font-bold ">Consequences Without Representation:</span> Without the expertise of an experienced attorney, it’s tough to prove the claim. The evidence is often hidden, and facilities may try to deny their responsibility. Florida law allows a two-year statute of limitations to file a claim. Missing this deadline means losing the right to pursue justice.
+  </li>
+ </ul>
+</p>
+
+
+<p class="text-base md:text-lg font-bold mb-1  pl-5">
+Carter Injury Law: Committed to Securing the Justice You Deserve
+</p>
+
+<p className="text-base md:text-lg">
+  <ul class="list-disc pl-5">
+
+  <li>We’ll review medical records, staff schedules, facility conditions, and witness testimonies to uncover the truth behind the neglect or abandonment.
+  </li>
+
+  <li>Our team is skilled in Florida’s nursing home laws and regulations, and we’ll use this knowledge to build a solid case and hold the facility accountable.
+  </li>
+
+  <li>Whether negotiating a settlement or going to trial, we’ll fight for the compensation your family deserves and ensure the nursing home is held responsible.
+  </li>
+
+
+  <li>We understand the emotional toll of neglect or abandonment. Let us handle the legal details while you focus on your family’s well-being. Contact us for a free consultation without breaking the bank—no fee unless we win.
+  </li>
+
+  </ul>
+</p>
+
+</div>
+`,
+  },
+
+  {
+    icon: "/assets/servicepage/icon/auto-accidents.png",
+    title: "Sexual Abuse",
+    slug: "sexual-abuse",
+    details: `<div className="w-full flex flex-col gap-2 lg:gap-4">
+<h2 className="text-stone-950 font-semibold text-3xl md:text-4xl lg:text-5xl">
+Sexual Abuse
+</h2>
+<img
+  src="/assets/servicepage/nursing-home-abuse/sexual-abuse.jpg"
+  alt="Sexual Abuse"
+  className="w-full py-3"
+/>
+
+<p className="text-base md:text-lg">
+Sexual abuse in nursing homes involves unwanted sexual contact or advances by staff, residents, or visitors. This abuse, whether physical, verbal, or emotional, often goes unreported due to victims' vulnerability. Florida law requires nursing homes to protect residents from all forms of abuse and maintain proper safety measures. When sexual abuse occurs, those responsible—individuals or the facility—can be held legally accountable.
+</p>
+
+<p class= "text-base md:text-lg font-bold mb-1  pl-5">
+    Who Should File a Claim and Why Having an Attorney Is Essential 
+</p>
+
+<p class="text-base md:text-lg ">
+ <ul class="list-disc pl-5">
+<li><span class= " font-bold ">Benefits of Filing:</span>Victims and families of nursing home sexual abuse have the right to seek compensation for physical, emotional, and psychological injury, such as medical fees, counseling, pain and suffering, and punitive penalties. Filing a claim can also help avoid future abuse and reveal misconduct.
+</li>
+
+ <li><span class= " font-bold ">Consequences Without Representation:</span> The delicate nature of sexual assault in a nursing home makes it difficult to prove. Without legal assistance, victims may struggle to gather evidence, counter nursing home's defenses, and go through complicated legal procedures. Florida's two-year statute of limitations mandates prompt action, and without guidance from professionals, victims risk losing their opportunity at justice.
+  </li>
+ </ul>
+</p>
+
+
+<p class="text-base md:text-lg font-bold mb-1  pl-5">
+Carter Injury Law: Committed to Securing the Justice You Deserve
+</p>
+
+<p className="text-base md:text-lg">
+  <ul class="list-disc pl-5">
+
+  <li>We’ll review medical records, staff schedules, facility conditions, and witness We’ll entirely investigate your case, from medical records, and interviewing witnesses to collaborating with experts to gather evidence of the abuse, always treating your situation with the utmost sensitivity.
+  </li>
+
+  <li>With extensive experience in Florida nursing home abuse cases, we navigate complex legal requirements to prove sexual abuse claims effectively.
+  </li>
+
+  <li>We will hold abusers and the facility accountable, seeking full compensation for the emotional, physical, and psychological harm caused. Whether negotiating or going to trial, we fight for the victim’s rights.
+  </li>
+
+
+  <li>We know these cases are deeply sensitive. Let us help you seek justice—contact us for a free, confidential consultation. We take no fees unless we win.
+  </li>
+
+  </ul>
+</p>
+
+</div>
+`,
+  },
+
+  {
+    icon: "/assets/servicepage/icon/auto-accidents.png",
+    title: "Financial Abuse",
+    slug: "financial-abuse",
+    details: `<div className="w-full flex flex-col gap-2 lg:gap-4">
+<h2 className="text-stone-950 font-semibold text-3xl md:text-4xl lg:text-5xl">
+Financial Abuse
+</h2>
+<img
+  src="/assets/servicepage/nursing-home-abuse/financial-abuse.jpg"
+  alt="Financial Abuse"
+  className="w-full py-3"
+/>
+
+<p className="text-base md:text-lg">
+Sexual abuse in nursing homes involves unwanted sexual contact or advances by staff, residents, or visitors. This abuse, whether physical, verbal, or emotional, often goes unreported due to victims' vulnerability. Florida law requires nursing homes to protect residents from all forms of abuse and maintain proper safety measures. When sexual abuse occurs, those responsible—individuals or the facility—can be held legally accountable.
+</p>
+
+<p class= "text-base md:text-lg font-bold mb-1  pl-5">
+    Who Should File a Claim and Why Having an Attorney Is Essential 
+</p>
+
+<p class="text-base md:text-lg ">
+ <ul class="list-disc pl-5">
+<li><span class= " font-bold ">Benefits of Filing:</span>Victims and families of nursing home sexual abuse have the right to seek compensation for physical, emotional, and psychological injury, such as medical fees, counseling, pain and suffering, and punitive penalties. Filing a claim can also help avoid future abuse and reveal misconduct.
+</li>
+
+ <li><span class= " font-bold ">Consequences Without Representation:</span> The delicate nature of sexual assault in a nursing home makes it difficult to prove. Without legal assistance, victims may struggle to gather evidence, counter nursing home's defenses, and go through complicated legal procedures. Florida's two-year statute of limitations mandates prompt action, and without guidance from professionals, victims risk losing their opportunity at justice.
+  </li>
+ </ul>
+</p>
+
+
+<p class="text-base md:text-lg font-bold mb-1  pl-5">
+Carter Injury Law: Committed to Securing the Justice You Deserve
+</p>
+
+<p className="text-base md:text-lg">
+  <ul class="list-disc pl-5">
+
+  <li>We’ll review medical records, staff schedules, facility conditions, and witness We’ll entirely investigate your case, from medical records, and interviewing witnesses to collaborating with experts to gather evidence of the abuse, always treating your situation with the utmost sensitivity.
+  </li>
+
+  <li>With extensive experience in Florida nursing home abuse cases, we navigate complex legal requirements to prove sexual abuse claims effectively.
+  </li>
+
+  <li>We will hold abusers and the facility accountable, seeking full compensation for the emotional, physical, and psychological harm caused. Whether negotiating or going to trial, we fight for the victim’s rights.
+  </li>
+
+
+  <li>We know these cases are deeply sensitive. Let us help you seek justice—contact us for a free, confidential consultation. We take no fees unless we win.
+  </li>
+
+  </ul>
+</p>
+
+</div>
+`,
+  },
+];
