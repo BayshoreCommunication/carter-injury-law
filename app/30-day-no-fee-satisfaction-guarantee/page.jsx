3eb6a05b8334341e6270009f9c2ebca08f18091f
arsahak <arsahak99@gmail.com>
2024-11-13 14:12:57 +0600

    update metadata

--- app/30-day-no-fee-satisfaction-guarantee/page.jsx
+++ app/30-day-no-fee-satisfaction-guarantee/page.jsx
@@ -9,21 +9,14 @@ import CallToAction from "@/components/shared/CallToAction";
 import PracticeAreasSection from "@/components/shared/PracticeAreasSection";
 import Head from "next/head";
 
+export const metadata = {
+  title: "demo metadata",
+  description: "demo meta title",
+};
+
 const page = () => {
   return (
     <>
-      <Head>
-        <title> 30-Day No-Fee Satisfaction Guarantee - Carter Injury Law</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content={`At Carter Injury Law, you’re family. We offer open lines of communication to foster personal relationships and optimize case outcomes for our clients. `}
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Hand:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <HeroSection />
       <SatisfactionGuarantee />
       <PracticeAreasSection />
--- app/page.js
+++ app/page.js
@@ -6,6 +6,21 @@ import CallToAction from "@/components/shared/CallToAction";
 import PracticeAreasSection from "@/components/shared/PracticeAreasSection";
 import TestimonialSlideSection from "@/components/shared/TestimonialSlideSection";
 
+export const metadata = {
+  title: "Carter Injury Law",
+  description:
+    "At Carter Injury Law, you’re family. We offer open lines of communication to foster personal relationships and optimize case outcomes for our clients. Every client gets the personal cell phone number of the attorney.",
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-USA",
+    },
+  },
+  openGraph: {
+    images: "/opengraph-image.jpg",
+  },
+};
+
 export default function Home() {
   return (
     <main className="w-full">
