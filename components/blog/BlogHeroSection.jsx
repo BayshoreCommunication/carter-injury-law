0ba56b8c03cdd5410044874b3b76355e28973914
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2024-12-19 10:23:57 +0600

    updated h of blog

--- components/blog/BlogHeroSection.jsx
+++ components/blog/BlogHeroSection.jsx
@@ -5,7 +5,7 @@ import SectionLayout from "../shared/SectionLayout";
 const BlogHeroSection = () => {
   return (
     <div>
-      <div className="relative w-full h-[400px] md:h-[500px] lg:h-[650px] xl:h-[750px]">
+      <div className="relative w-full h-[300px] md:h-[500px] lg:h-[650px] xl:h-[750px]">
         <Image
           src="/assets/shared/practice-area-bg.png"
           alt="testimonials Bg"
@@ -16,7 +16,7 @@ const BlogHeroSection = () => {
           className="z-0 object-cover w-full h-full object-bottom"
         />
 
-        <div className="absolute inset-0 flex justify-center items-center z-10 lg:h-[650px]">
+        <div className="absolute inset-0 flex justify-center items-center z-10 lg:h-[450px] xl:h-[550px]">
           <h1 className="text-white font-semibold text-3xl md:text-5xl lg:text-[80px] text-center uppercase drop-shadow-[1px_0_10px_#000000]">
             blog
           </h1>
@@ -24,7 +24,7 @@ const BlogHeroSection = () => {
         {/* <div className="absolute -bottom-52 right-[15%] hidden lg:block">
           <HeaderForm className="bg-white" />
         </div> */}
-        <div className="absolute scale-90 xl:scale-100 -bottom-60 xl:-bottom-52 right-[12%] hidden lg:block ">
+        <div className="absolute scale-100 -bottom-56 lg:-bottom-48 xl:-bottom-48  right-[12%] hidden lg:block ">
           <HeaderForm className="bg-white" />
         </div>
       </div>
