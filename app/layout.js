087e0bf2cae342eb58f68a0e2a784a1e23360414
arsahak <arsahak99@gmail.com>
2025-05-05 15:07:02 +0600

    feat: add Facebook Pixel tracking script for enhanced analytics

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import BlogPopupDesign from "@/components/shared/BlogPopupDesign";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "./providers";
 
@@ -20,6 +21,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Facebook Pixel Script */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1171875651343659');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* NoScript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1171875651343659&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={montserrat.className}>
         <Providers>
           <div className="overflow-x-hidden text-black bg-white">
