731a4e955653c36ea7a929fa7a734b2315a75eb9
BayshoreCommunication <134770953+BayshoreCommunication@users.noreply.github.com>
2025-11-02 17:25:08 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -84,11 +84,11 @@ export default function RootLayout({ children }) {
 
             {/* AI Chatbot Widget */}
 
-            <Script
-              src="https://aibotwidget.bayshorecommunication.org/chatbot-widget.min.js"
-              data-api-key="org_sk_3ca4feb8c1afe80f73e1a40256d48e7c"
-              async
-            />
+            // <Script
+            //   src="https://aibotwidget.bayshorecommunication.org/chatbot-widget.min.js"
+            //   data-api-key="org_sk_3ca4feb8c1afe80f73e1a40256d48e7c"
+            //   async
+            // />
                 
           </div>
         </Providers>
