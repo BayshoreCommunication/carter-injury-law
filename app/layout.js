056aaabb7675c4348ba427c5a2dcd1a27fbe11cd
arsahak <arsahak99@gmail.com>
2025-11-06 14:59:05 +0600

    update widget url setup

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-import ChatbotWidget from "@/components/ChatbotWidget";
 import Footer from "@/components/Footer";
 import MainNavbar from "@/components/MainNavbar";
 import BlogPopupDesign from "@/components/shared/BlogPopupDesign";
@@ -84,7 +83,18 @@ export default function RootLayout({ children }) {
             <Footer />
 
             {/* AI Chatbot Widget */}
-            <ChatbotWidget />
+
+            <Script
+              src="https://aibotwidget.bayshorecommunication.org/chatbot-widget.min.js"
+              data-api-key="org_sk_3ca4feb8c1afe80f73e1a40256d48e7c"
+              async
+            />
+
+            {/* <script
+              src="https://aibotwidget.bayshorecommunication.org/chatbot-widget.min.js"
+              data-api-key="org_sk_8d9205fcee1781de88f8718b04ad9167"
+              async
+            ></script> */}
           </div>
         </Providers>
       </body>
